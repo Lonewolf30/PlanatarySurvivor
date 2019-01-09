8cf88960f159ba1cf1ae123a12215bb67ecfcf02
Lonewolf <lonewolf.30303@gmail.com>
2019-01-09 16:59:57 +0200

    Init Commit

--- src/com/lonewolf/PS/Engine/Component/Gui/Addon/GUIText.java
+++ src/com/lonewolf/PS/Engine/Component/Gui/Addon/GUIText.java
@@ -2,29 +2,27 @@ package com.lonewolf.PS.Engine.Component.Gui.Addon;
 
 import com.lonewolf.PS.Engine.Component.Gui.GUIAddon;
 import com.lonewolf.PS.Engine.Core.Vector3f;
+import com.lonewolf.PS.Engine.Render.RenderHelper.TextRender;
 import com.lonewolf.PS.Engine.Render.RenderingEngine;
 import com.lonewolf.PS.Engine.Render.Shader;
-import com.lonewolf.PS.Engine.Render.TextRender;
 
 public class GUIText extends GUIAddon
 {
-    private String text;
+    private TextRender textRender;
     private float x;
     private float y;
-    private float width;
     private float scale;
 
-    public GUIText(String text, float x, float y, float width, float scale) {
-        this.text = text;
+    public GUIText(String text, float x, float y, float scale, boolean changes) {
+        this.textRender = new TextRender(text, changes);
         this.x = x;
         this.y = y;
-        this.width = width;
         this.scale = scale;
     }
 
     public void setText(String text)
     {
-        this.text = text;
+        this.textRender.setText(text);
     }
 
     @Override
@@ -32,6 +30,6 @@ public class GUIText extends GUIAddon
     {
         shader.bind();
         shader.SetUniform("color", new Vector3f(1,0,1));
-        TextRender.drawString(text, x, y, scale, width);
+
     }
 }
---
+++ src/com/lonewolf/PS/Engine/Render/RenderHelper/TextRender.java
@@ -0,0 +1,38 @@
+package com.lonewolf.PS.Engine.Render.RenderHelper;
+
+import com.lonewolf.PS.Engine.Render.Texture;
+
+import java.awt.image.BufferedImage;
+
+public class TextRender
+{
+    private String text;
+    private boolean textChanges;
+    private BufferedImage image;
+
+    public TextRender(String text, boolean textChanges)
+    {
+        this.text = text;
+        this.textChanges = textChanges;
+        generateBufferedImage();
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+
+    private void generateBufferedImage()
+    {
+        BufferedImage image = new BufferedImage(text.length()*8,8,1);
+
+
+    }
+
+    public Texture getTextTexture()
+    {
+        if (textChanges)
+            generateBufferedImage();
+        return new Texture(image);
+    }
+}
